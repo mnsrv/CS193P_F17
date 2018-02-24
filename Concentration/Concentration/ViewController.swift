cb07fa6cbddd9f4a1fb6a89e91f836a75a64778d
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-02-24 03:08:32 +0300

    1. Introduction to iOS 11, Xcode 9 and Swift 4

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Aleksandr on 24.02.2018.
+//  Copyright © 2018 Aleksandr Mansurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+}
